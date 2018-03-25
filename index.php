cf79d8b1b2368363301d28c5286a887155b4fc89
Akshay Khairmode <akshay.k.osmos@gmail.com>
2018-03-26 00:26:46 +0530

    Adding shuttle-export library

--- index.php
+++ index.php
@@ -4,65 +4,120 @@ Author :- Akshay Khairmode
 */
 namespace DB_INIT;
 
-require './functions.php';
+require './functions.php';// Used for debugging purpose.
 use Func as F;
 
 class Database {
 
-  private $hostname = 'localhost';
-  private $db_user = 'root';
-  private $db_password = '';
-  private $database = 'ciltest_cqms';
+  private $config_path = "config.json";
 
-  private $tables = array("cqms_category_master"=>true,"cqms_channel_master"=>true,"cqms_escalation_master"=>false);
+  private $hostname,$db_user,$db_password,$database,$tables;
 
   public function __construct() {
-      mysql_connect($this->hostname,$this->db_user,$this->db_password);
-      mysql_select_db($this->database) or F\mdie();
-  }
 
-  private function ParseStructure () {
+      $file_content = file_get_contents($this->config_path);
 
-      $query_fire = mysql_query("SHOW CREATE TABLE cqms_category_master") or F\mdie();
+      $parse_file = json_decode($file_content,true);
 
-      while ($res = mysql_fetch_assoc($query_fire)) {
-          F\pp($res);
+      if(!$parse_file) {
+        die("Could Not read config file");
       }
-  }
 
-  private function ParseBoth () {
+      //Initializing
+      $config = $parse_file['config'];
+      $this->tables = $parse_file['tables'];
+      //End
 
-      $query_fire = mysql_query("SHOW CREATE TABLE cqms_category_master") or F\mdie();
-
-      while ($res = mysql_fetch_assoc($query_fire)) {
-          F\pp($res);
-      }
-  }
-
-  public function ExecuteInstallScript () {
+      mysql_connect($config['host'],$config['username'],$config['password']);
+      mysql_select_db($config['database']) or F\mdie();
 
   }
 
-  public function GetTable () {
+  private function getTableBoth () {//Returns the tables which need both structure and data
+
+    $return = array();
 
     foreach ($this->tables as $table_name => $status) {
-
-        if ($status == true) {//If data is required
-
-          
-
-        } elseif($status == false) {
-
-          
-          
-        }
+      if ($status == true) {
+        array_push($return, $table_name);
+      }
     }
+
+    return $return;
+
+  }
+
+
+  private function createFileStructure ($filename,$content) {//Insert structure of each file.
+
+    $path = "./storage/structure/".$filename.".txt";
+
+    $myfile = fopen($path, "w") or die("Unable to open file!");
+    fwrite($myfile, $content);
+    fclose($myfile);
+
+  }
+
+  private function createFileFull () {
+
+  }
+
+  private function parseStructure () {
+
+      foreach ($this->tables as $table_name => $status) {
+        
+        $query_fire = mysql_query("SHOW CREATE TABLE ".$table_name) or F\mdie();
+
+        $res = mysql_fetch_assoc($query_fire);
+
+        $this->createFileStructure($res['Table'],$res['Create Table']);
+
+      }
+
+  }
+
+  private function parseData ($table_name) {
+
+      $filename = $table_name.".csv";
+
+      echo $query = "SELECT * INTO OUTFILE \".".DIRECTORY_SEPARATOR ."storage".DIRECTORY_SEPARATOR ."full".DIRECTORY_SEPARATOR ."test.csv\"\n"
+      . "FIELDS TERMINATED BY \',\' OPTIONALLY ENCLOSED BY \'\"\'\n"
+      . "LINES TERMINATED BY \"\\n\"\n"
+      . "FROM cqms_escalation_master";
+
+      // echo $query = "SELECT * INTO OUTFILE \"./storage/full/test.csv\"\"\n"
+      // . "FIELDS TERMINATED BY \',\' OPTIONALLY ENCLOSED BY \'\"\'\n"
+      // . "LINES TERMINATED BY \"\\n\"\n"
+      // . "FROM ".$table_name;
+
+      // $query_fire = mysql_query($query) or F\mdie();
+
+  }
+
+  public function install () {
+
+  }
+
+  public function backup () {
+
+    $this->parseStructure();//Creates txt files containing the structure code
+
+    mysql_query("START TRANSACTION");
+
+    foreach ($this->getTableBoth() as $table_name) {
+        $this->parseData($table_name);
+    }
+
+    mysql_query("COMMIT");
     
   }
 
 }
 
+
+;
+
 $obj = new Database;
-$obj->GetTable();
+$obj->backup();
 
 ?>
